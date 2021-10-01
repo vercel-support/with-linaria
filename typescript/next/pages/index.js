10e8a275cb268fa6070b579e4454f664bd6e58e5
Nicholas Rempel <nick@lucent.is>
2021-10-01 11:49:23 -0700

    style change

--- typescript/next/pages/index.js
+++ typescript/next/pages/index.js
@@ -3,8 +3,8 @@ import { styled } from 'linaria/react'
 import { css } from 'linaria'
 
 const Box = styled.div`
-  margin-top: 70px;
-  margin-left: 70px;
+  margin-top: 80px;
+  margin-left: 80px;
   height: 200px;
   width: 200px;
   background-color: tomato;
